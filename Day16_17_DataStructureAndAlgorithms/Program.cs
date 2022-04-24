b2f520e033b5dfe7d8c1a048b46e50fd45d9ae33
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-04-24 18:36:50 +0530

    [Ronit]Added:Binary Search

---
+++ Day16_17_DataStructureAndAlgorithms/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16_17_DataStructureAndAlgorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nHow many words you want to add : ");
+            int noOfWords = Convert.ToInt32(Console.ReadLine());
+            string[] words = new string[noOfWords];
+            Console.WriteLine("\nEnter words one by one : ");
+            for (int i = 0; i < noOfWords; i++)
+            {
+                words[i] = Console.ReadLine();
+            }
+            Console.WriteLine("\nWhich word you want to search : ");
+            string w = Console.ReadLine();
+            int pos = BinarySearch.searchWord(words, w);
+            if (pos == -1)
+            {
+                Console.WriteLine($"\n\"{w}\" word not found");
+            }
+            else
+                Console.WriteLine($"\n\"{w}\" word founded");
+            
+        }
+    }
+}
